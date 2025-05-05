384d55abf25d1a41a384b91b55c8924f34d686a6
= <redman29h@gmail.com>
2025-05-05 23:46:27 +1000

    refactor: remove unused post router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 import { emailRouter } from "./routers/email";
 
@@ -8,7 +7,6 @@ import { emailRouter } from "./routers/email";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   email: emailRouter,
 });
 
